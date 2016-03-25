29a859f5ae82056335cfef567f867783d79a11ca
Maksim Puganov <aquariustech@yandex.ru>
2016-03-25 14:02:11 +0300

    finished contact page add google map with google javascript api

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -6,14 +6,16 @@ var showMenu = (function () {
 
     var _setUpListners = function () {
         // прослушка событий
-        document.getElementById("sidebar-toggle").addEventListener("click", toogleMenu);
-        function toogleMenu() {
-            document.querySelector(".wrapper").classList.toggle("open-sidebar");
-        };
+        // document.getElementById("sidebar-toggle").addEventListener("click", toogleMenu);
+        // function toogleMenu() {
+        //     document.querySelector(".wrapper").classList.toggle("open-sidebar");
+        // };
 
         function init_map() {
             var myOptions = {
                 zoom:16,
+                disableDefaultUI: true,
+                scrollwheel: false,
                 center:new google.maps.LatLng(42.2616788,-85.61586119999998),
                 mapTypeId: google.maps.MapTypeId.ROADMAP};
 
--- dev/scripts/map.js
+++
@@ -1,9 +0,0 @@
-function init_map() {
-    var myOptions = {
-        zoom:16,
-        center:new google.maps.LatLng(42.2616788,-85.61586119999998),
-        mapTypeId: google.maps.MapTypeId.ROADMAP};
-
-        map = new google.maps.Map(document.getElementById('gmap_canvas'),myOptions);
-    }
-google.maps.event.addDomListener(window, 'load', init_map)
